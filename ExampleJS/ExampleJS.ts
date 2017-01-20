8638c21590857583863b8d17dbe0750484107dfd
Ernst Hot <thorsten@oerts.dk>
2017-01-20 14:44:18 +0100

    Example showing how to assign properties to objects of type Global added

--- ExampleJS/ExampleJS.ts
+++ ExampleJS/ExampleJS.ts
@@ -9,6 +9,12 @@ function bang() {
 	let theObject = new em.TheClass(42);
 	post("theObject.getIndex(): " + theObject.getIndex() + "\n");
 	post("The square of pi is " + em.square(Math.PI) + "\n");
+
+	// Cast to <any> to assign properties to objects of type Global.
+	var g = new Global("");
+	(<any>g).newProperty = "I am new.";
+
+	post("(<any>g).newProperty: " + (<any>g).newProperty + "\n");
 }
 
 function msg_float(v)
